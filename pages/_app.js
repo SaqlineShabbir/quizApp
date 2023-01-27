a98885f357b348cd9163785745113595d36ce6ae
Saqline Shabbir <saqline5869@gmail.com>
2023-01-27 23:00:21 +0600

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import { SessionProvider } from 'next-auth/react';
 import { Provider } from 'react-redux';
+import DashboardProvider from '../components/dashboard/provider/context';
 import AuthProvider from '../context/AuthProvider';
 import { store } from '../redux/app/store';
 import '../styles/globals.css';
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <AuthProvider>
-      <SessionProvider session={session}>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </SessionProvider>
-    </AuthProvider>
+    <DashboardProvider>
+      <AuthProvider>
+        <SessionProvider session={session}>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </SessionProvider>
+      </AuthProvider>
+    </DashboardProvider>
   );
 }
 
