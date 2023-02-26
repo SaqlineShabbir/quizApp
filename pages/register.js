45c31a028f19f15b164e29fde7c27432a73e8e32
Saqline Shabbir <saqline5869@gmail.com>
2023-02-26 09:24:10 +0600

    design changes in login  and sign uppage

--- pages/register.js
+++ pages/register.js
@@ -27,9 +27,9 @@ const Register = () => {
   };
 
   return (
-    <div className="lg:flex">
-      <div className="bg-red-100  lg:w-1/2 h-[100vh]">
-        <div className="flex  justify-center items-center gap-10 py-[300px] lg:px-[200px]  md:px-[70px]  px-10">
+    <div className="lg:flex overflow-y-scroll min-h-full w-[100%]">
+      <div className="bg-red-100 lg:w-[50%] flex flex-col justify-center items-center px-20 lg:py-0 py-10 ">
+        <div className=" ">
           <div className="space-y-5">
             <p className="">
               Those people who develop the ability to continuously acquire new
@@ -41,9 +41,9 @@ const Register = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white  lg:w-1/2  px-20">
+      <div className="lg:px-20 px-3 lg:w-[50%] ">
         <p className="mt-20">Back To Topify</p>
-        <div className="flex flex-col justify-center items-center gap-10 mt-24">
+        <div className="flex flex-col justify-center items-center gap-10 mt-10">
           <p className="flex  items-center lg:text-4xl text-2xl font-bold">
             Sign Up
           </p>
@@ -52,23 +52,20 @@ const Register = () => {
             <div className="space-y-5">
               <input
                 type="text"
-                id="first_name"
-                className="lg:w-[400px]  py-5 px-5 drop-shadow-2xl  text-gray-900 text-sm rounded-full  "
+                className="lg:w-[400px] w-[350px] py-5 px-5 drop-shadow-2xl  text-gray-900 text-sm rounded-full  "
                 placeholder="Full Name"
                 onChange={(e) => setName(e.target.value)}
                 required
               />
               <input
                 type="email"
-                id="email"
-                className="lg:w-[400px]  py-5 px-5 drop-shadow-2xl  text-gray-900 text-sm rounded-full  "
+                className="lg:w-[400px]  w-[350px]  py-5 px-5 drop-shadow-2xl  text-gray-900 text-sm rounded-full  "
                 placeholder="Email"
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
               <input
                 type="password"
-                id="password"
                 className=" lg:w-[400px]  py-5 px-5 drop-shadow-xl  text-gray-900 text-sm rounded-full block w-full p-2.5  "
                 placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
@@ -76,7 +73,6 @@ const Register = () => {
               />
               <input
                 type="password"
-                id="password"
                 className=" lg:w-[400px]  py-5 px-5 drop-shadow-xl  text-gray-900 text-sm rounded-full block w-full p-2.5  "
                 placeholder="Confirm Password"
                 onChange={(e) => setConfirmPassword(e.target.value)}
